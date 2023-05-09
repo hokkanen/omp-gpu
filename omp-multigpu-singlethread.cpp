796657f906d1a4d306dc67e6e6551efb9cacc839
Jaro Hokkanen <jaro.hokkanen@csc.fi>
2023-05-09 10:02:57 +0300

    Change define size to const int size

--- omp-multigpu-singlethread.cpp
+++ omp-multigpu-singlethread.cpp
@@ -3,19 +3,18 @@
 #include <time.h>
 #include <omp.h>
 
-#define SIZE 1000000
-
 int main(int argc, char *argv[]){
 
-    int A[SIZE];
-    int num_gpus = omp_get_num_devices();
+    const int num_gpus = omp_get_num_devices();
+    const int size = 1e6;
+    int A[size];
 
     clock_t t_start = clock();
     for(int n = 0; n < num_gpus; n++)
     {
         omp_set_default_device(n);
-        #pragma omp target teams distribute parallel for map(from:A[n * (SIZE / 4):SIZE/4]) nowait
-        for (int i = n * (SIZE / 4); i < (n + 1) * (SIZE / 4); i++) {
+        #pragma omp target teams distribute parallel for map(from:A[n * (size / 4):size/4]) nowait
+        for (int i = n * (size / 4); i < (n + 1) * (size / 4); i++) {
             A[i] = i;
         }
         double t_s = (double) (clock() - t_start) / CLOCKS_PER_SEC;
@@ -25,5 +24,5 @@ int main(int argc, char *argv[]){
 
     #pragma omp taskwait
     for (int n = 0; n < num_gpus; n++)
-        printf("CPU thread %d, post-loop value from GPU %d: %d\n", omp_get_thread_num(), n, A[SIZE / 4 * (n + 1) - 1]);
+        printf("CPU thread %d, post-loop value from GPU %d: %d\n", omp_get_thread_num(), n, A[size / 4 * (n + 1) - 1]);
 }
